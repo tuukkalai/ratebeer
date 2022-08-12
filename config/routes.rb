14938a6b01e440d5445ffe3a3073ae0eadd6036a
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-08-12 18:38:56 +0300

    Ex. 4 + seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :beers
   resources :breweries
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_12_142142) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_12_151525) do
+  create_table "beers", force: :cascade do |t|
+    t.string "name"
+    t.string "style"
+    t.integer "brewery_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "breweries", force: :cascade do |t|
     t.string "name"
     t.integer "year"
