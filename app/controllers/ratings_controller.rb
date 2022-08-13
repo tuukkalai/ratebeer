b241a1d4164999046f089278be7cc49a19c6c7ff
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-08-14 00:13:23 +0300

    Ex.3 List ratings in human readable format

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,5 +1,14 @@
 class RatingsController < ApplicationController
+  before_action :set_rating, only: %i[ show ]
 	def index
 		@ratings = Rating.all
 	end
+
+  def show
+  end
+
+  private
+    def set_rating
+      @rating = Rating.find(params[:id])
+    end
 end
