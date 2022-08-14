644b316609ca9fe8c8ae10bd581c52f39cbd7082
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-08-14 21:52:22 +0300

    Add create new rating form

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -7,6 +7,15 @@ class RatingsController < ApplicationController
   def show
   end
 
+  def new
+    @rating = Rating.new
+  end
+
+  def create
+    Rating.create params.require(:rating).permit(:beer_id, :score)
+    redirect_to ratings_path
+  end
+
   private
     def set_rating
       @rating = Rating.find(params[:id])
