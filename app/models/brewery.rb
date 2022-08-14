8bf09359e8f96ff8f33a265be5cedef573a2f39d
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-08-15 00:50:41 +0300

    Ex.14 Average rating data to brewery page

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,10 @@
 class Brewery < ApplicationRecord
 	has_many :beers, dependent: :destroy
+	has_many :ratings, through: :beers
+
+	def average_rating
+		if !self.ratings.empty?
+			self.ratings.average(:score)
+		end
+	end
 end
