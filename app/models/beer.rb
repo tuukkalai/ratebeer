80b19ce190013a60cc30619a26301d1195ea13f0
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-08-14 11:36:18 +0300

    Ex.4 Add average rating to beers

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,11 @@
 class Beer < ApplicationRecord
 	belongs_to :brewery
 	has_many :ratings
+
+	def average_rating
+		if !self.ratings.empty?
+			(self.ratings.sum(:score).to_f/self.ratings.count).round(2)
+		end
+	end
+
 end
