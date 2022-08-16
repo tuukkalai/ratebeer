eeaa4b1cb2f90cb42aab21a27e3b67916b7ed405
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-08-16 21:37:45 +0300

    Ex.2 Remove the too long example method

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -4,29 +4,6 @@ class Beer < ApplicationRecord
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
 
-  def way_too_long_method
-    x = 2
-    if x == 1
-      puts "It is 1"
-    elsif x < 0
-      puts "What"
-    elsif x > 1010
-      puts "ridiculousness"
-    elsif x > 2
-      puts "is"
-    elsif x < 2
-      puts "going"
-    elsif x % 551 == 0
-      puts "on"
-    elsif x**2 > x**x
-      puts "in"
-    elsif x != 0
-      puts "here"
-    else
-      puts "?"
-    end
-  end
-
   def to_s
     "#{name}, #{brewery.name}"
   end
