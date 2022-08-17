f70e3fa862323f3b2972493ec6c6f4c9ccff3273
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-08-17 21:48:15 +0300

    Ex.3 Add user, session and login/signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
   resources :beers
   resources :breweries
+  resource :session, only: [:new, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -12,4 +14,5 @@ Rails.application.routes.draw do
   # get "ratings/new", to: "ratings#new"
   # post "ratings", to: "ratings#create"
   resources :ratings, only: [:index, :new, :create, :destroy]
+  get "signup", to: "users#new"
 end
