04903bd634b9fca3c8b8b2e4eedf82ad0639e9de
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-08-21 20:59:02 +0300

    Ex.6 Update delete links on ratings page to navigate to user page

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -13,9 +13,11 @@ class RatingsController < ApplicationController
   end
 
   def create
-    rating = Rating.create params.require(:rating).permit(:beer_id, :score)
-    session[:last_rating] = "#{rating.beer.name}, #{rating.score} points"
-    redirect_to ratings_path
+    rating = Rating.new params.require(:rating).permit(:beer_id, :score)
+    rating.user = current_user
+    rating.save
+    # session[:last_rating] = "#{rating.beer.name}, #{rating.score} points"
+    redirect_to current_user
   end
 
   def destroy
