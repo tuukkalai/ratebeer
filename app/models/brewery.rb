6d2a94db05ea1d500c2e64948a6119ee6e2f0258
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-08-26 10:12:50 +0300

    Ex.8 Brewery cannot be established in the future

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -6,6 +6,12 @@ class Brewery < ApplicationRecord
 
   validates :name, presence: true
   validates :year, numericality: { greater_than_or_equal_to: 1040,
-                                   less_than_or_equal_to: 2022,
                                    only_integer: true }
+  validate :brewery_not_established_in_future
+
+  def brewery_not_established_in_future
+    if year >= Time.now.year
+      errors.add(:year, "can't be in the future")
+    end
+  end
 end
