5a13ce7f55a204ea4c00917cdfcf5db50849bc99
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-09-06 21:45:39 +0300

    Ex.1 Add faulty password tests to User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,18 @@ RSpec.describe User, type: :model do
     expect(User.count).to eq(0)
   end
 
+  it "is not saved with short password" do
+    user = User.create({username: "Pekka", password: "P3x", password_confirmation: "P3x"})
+    expect(user.valid?).to be(false)
+    expect(User.count).to eq(0)
+  end
+
+  it "is not saved with small letter password" do
+    user = User.create({username: "Pekka", password: "p3kkanen", password_confirmation: "p3kkanen"})
+    expect(user.valid?).to be(false)
+    expect(User.count).to eq(0)
+  end
+
   describe "with a proper password" do
     let(:user){ User.create({username: "Pekka", password: "5Alasana", password_confirmation: "5Alasana"}) }
     let(:test_brewery) { Brewery.new({name: "test", year: 2000}) }
@@ -35,4 +47,5 @@ RSpec.describe User, type: :model do
       expect(user.average_rating).to eq(15.0)
     end
   end
+
 end
