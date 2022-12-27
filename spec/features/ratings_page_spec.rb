cded152a744701f18400985a967a2453124f7fc9
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2022-12-27 13:15:23 +0200

    v4teht7: ratings visibility on users page

--- spec/features/ratings_page_spec.rb
+++ spec/features/ratings_page_spec.rb
@@ -6,7 +6,13 @@ describe "Rating" do
   let!(:brewery) { FactoryBot.create :brewery, name: "Koff" }
   let!(:beer1) { FactoryBot.create :beer, name: "iso 3", brewery:brewery }
   let!(:beer2) { FactoryBot.create :beer, name: "Karhu", brewery:brewery }
-  let!(:user) { FactoryBot.create :user }
+  let!(:user1) { FactoryBot.create :user }
+  let!(:user2) {
+    FactoryBot.create :user,
+    username: "Matti",
+    password: "5alaSana",
+    password_confirmation: "5alaSana"
+  }
 
   before :each do
     sign_in(username: 'Pekka', password: 'Foobar1')
@@ -21,13 +27,30 @@ describe "Rating" do
       click_button "Create Rating"
     }.to change{Rating.count}.from(0).to(1)
 
-    expect(user.ratings.count).to eq(1)
+    expect(user1.ratings.count).to eq(1)
     expect(beer1.ratings.count).to eq(1)
     expect(beer1.average_rating).to eq(15.0)
   end
 
-  it "page shows the amount of ratings correctly" do
-    create_multiple_ratings(beer1, 20, 1, 15, 9)
-    expect(page).to have_content "#{user.username} has made 4 ratings"
+  describe "is visible" do
+    before :each do
+      # user1 ratings
+      create_multiple_ratings(beer1, 21, 3, 15, 9)
+      click_link("Sign out")
+      # user2 ratings
+      sign_in(username: "Matti", password: "5alaSana")
+      create_multiple_ratings(beer2, 12, 2)
+    end
+
+    it "on user's page" do
+      expect(current_path).to eq(user_path(user2))
+      expect(page).to have_content "#{user2.username} has made 2 ratings"
+      expect(page).to have_content "#{beer2.name}"
+    end
+
+    it "only on user's page" do
+      expect(current_path).to eq(user_path(user2))
+      expect(page).to have_no_content "#{beer1.name}"
+    end
   end
 end
